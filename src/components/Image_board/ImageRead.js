36a78eec4d0df465207c59a06921ddeb8455806e
minzzweb <minzzweb@gmail.com>
2023-08-13 01:25:09 +0900

    이미지게시판 상세보기 프론트완료

--- src/components/Image_board/ImageRead.js
+++ src/components/Image_board/ImageRead.js
@@ -3,6 +3,10 @@ import { Link } from "react-router-dom";
 import Box from "@mui/material/Box";
 import Typography from "@mui/joy/Typography";
 import Input from "@mui/joy/Input";
+import cat from "../../assets/cat.png";
+import Button from "@mui/joy/Button";
+import Textarea from "@mui/joy/Textarea";
+import Table from "@mui/joy/Table";
 
 const ImageRead = ({ imageId, image, isLoading, categoryName }) => {
   // 이미지 표시 URL 생성
@@ -25,9 +29,7 @@ const ImageRead = ({ imageId, image, isLoading, categoryName }) => {
         sx={{
           color: "#EA9A3E",
         }}
-      >
-        유저 프로필 올 예정 <br />
-      </Box>
+      ></Box>
       {/* '로딩중...' 표시 */}
       {isLoading && "로딩중..."}
       {/* 상세보기 화면 표시 */}
@@ -40,38 +42,123 @@ const ImageRead = ({ imageId, image, isLoading, categoryName }) => {
             width: "100%",
           }}
         >
-          <table>
+          <Table>
             <tbody>
               <tr>
                 <td>
-                  <Input type="text" value={categoryName} />
+                  <Box
+                    sx={{
+                      borderRadius: "50%",
+                      border: "2px solid #A1E8A1",
+                      overflow: "hidden",
+                    }}
+                  >
+                    <img
+                      src={cat}
+                      style={{
+                        width: "50px",
+                        height: "50px",
+                        display: "block",
+                      }}
+                    />
+                  </Box>
+                </td>
+                <td colSpan={7}>
+                  {image.imageWriter} {""} 의<b> {""}Photo</b>
                 </td>
               </tr>
               <tr>
-                <td>
-                  <Input type="text" value={image.imageTitle} readOnly />
+                <td
+                  colSpan={2}
+                  style={{
+                    color: "#EA9A3E",
+                  }}
+                >
+                  {categoryName}{" "}
+                </td>
+                <td colSpan={5}> {image.imageTitle}</td>
+              </tr>
+
+              <tr>
+                <td colSpan="8">
+                  <Box
+                    sx={{
+                      width: "300px",
+                      height: "300px",
+                      display: "flex",
+                      justifyContent: "center",
+                      alignItems: "center",
+                      overflow: "hidden",
+                      margin: "0 auto",
+                    }}
+                  >
+                    <img
+                      src={pictureUrl()}
+                      alt=""
+                      style={{
+                        maxWidth: "100%",
+                        maxHeight: "100%",
+                      }}
+                    />
+                  </Box>
                 </td>
               </tr>
               <tr>
-                <td>미리보기</td>
-                <td>
-                  {/* 이미지 표시 */}
-                  <img src={pictureUrl()} alt="" width="200" />
-                </td>
-              </tr>
-              <tr>
-                <td>상품설명</td>
-                <td>
-                  {/* 상품설명 표시 */}
-                  <textarea value={image.imageContent} readOnly></textarea>
+                <td colSpan="8">
+                  <Textarea
+                    value={image.imageContent}
+                    readOnly
+                    sx={{
+                      height: "200px",
+                    }}
+                  ></Textarea>
                 </td>
               </tr>
             </tbody>
-          </table>
-          {/* 편집화면으로 이동 */}
-          <Link to={`/edit/${imageId}`}>편집</Link>
-          <button>삭제</button>
-          <Link to="/">목록</Link>
+          </Table>
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "flex-end",
+            }}
+          >
+            <Button
+              component={Link}
+              to="/edit/${imageId}"
+              size="sm"
+              sx={{
+                backgroundColor: "#CDD7E1",
+                height: "36px",
+              }}
+            >
+              {/*글쓴자만 보이게!*/}
+              편집
+            </Button>
+            <Button
+              component={Link}
+              to={"/image/list/" + categoryName}
+              size="sm"
+              sx={{
+                backgroundColor: "#CDD7E1",
+                height: "36px",
+                margin: "0px 3px",
+              }}
+            >
+              {/*글쓴자만 보이게!*/}
+              삭제
+            </Button>
+            <Button
+              component={Link}
+              to={"/image/list/" + categoryName}
+              size="sm"
+              sx={{
+                backgroundColor: "#CDD7E1",
+                height: "36px",
+              }}
+            >
+              목록
+            </Button>
+          </Box>
         </Box>
       )}
     </Box>
