d43e27d7f6698cdd521db2c27dbee5ccd7976857
minzzweb <minzzweb@gmail.com>
2023-08-14 03:34:56 +0900

    이미지 리스트 css

--- src/components/Image_board/ImagePosts.js
+++ src/components/Image_board/ImagePosts.js
@@ -3,36 +3,43 @@ import Box from "@mui/material/Box";
 import ImageList from "@mui/material/ImageList";
 import ImageListItem from "@mui/material/ImageListItem";
 import ImageListItemBar from "@mui/material/ImageListItemBar";
-import ListSubheader from "@mui/material/ListSubheader";
+
 import IconButton from "@mui/material/IconButton";
 import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
-import FavoriteIcon from "@mui/icons-material/Favorite";
+import Typography from "@mui/joy/Typography";
 
-const ImagePosts = ({ itemData }) => {
+const ImagePosts = ({ itemData, category }) => {
   return (
-    <Box sx={{ width: 1200, margin: "0 auto" }}>
+    <Box sx={{ width: 1200, margin: "0 auto", marginTop: "30px" }}>
       <ImageList sx={{ width: 1200, overflow: "hidden" }}>
         <ImageListItem key="Subheader" cols={4}>
-          <ListSubheader component="div">December</ListSubheader>
+          <Typography
+            level="h3"
+            sx={{
+              color: "#EA9A3E",
+              marginBottom: "30px",
+            }}
+          >
+            {category}
+          </Typography>
         </ImageListItem>
         {itemData.map((item) => (
           <ImageListItem key={item.img}>
             <img
               src={`${item.img}?w=300&fit=crop&auto=format`}
               srcSet={`${item.img}?w=300&fit=crop&auto=format&dpr=2 2x`}
-              alt={item.title}
+              alt={item.imageTitle}
               loading="lazy"
+              style={{ height: "300px", width: "300px" }}
             />
             <ImageListItemBar
-              title={item.title}
-              subtitle={item.author}
+              title={item.imageTitle}
+              subtitle={item.imageWriter}
               actionIcon={
                 <IconButton
                   sx={{ color: "rgba(255, 255, 255, 0.54)" }}
                   aria-label={`info about ${item.title}`}
-                >
-                  <FavoriteBorderIcon />
-                </IconButton>
+                ></IconButton>
               }
             />
           </ImageListItem>
--- src/containers/CategoryContainer.js
+++ src/containers/CategoryContainer.js
@@ -1,71 +1,73 @@
 import ImagePosts from "../components/Image_board/ImagePosts";
+import { useParams } from "react-router-dom";
 
 //더미 데이터
 const itemData = [
   {
     img: "https://images.unsplash.com/photo-1551963831-b3b1ca40c98e",
-    title: "Breakfast",
-    author: "@bkristastucchio",
+    imageTitle: "Breakfast",
+    imageWriter: "@bkristastucchio",
   },
   {
     img: "https://images.unsplash.com/photo-1551782450-a2132b4ba21d",
-    title: "Burger",
-    author: "@rollelflex_graphy726",
+    imageTitle: "Burger",
+    imageWriter: "@rollelflex_graphy726",
   },
   {
     img: "https://images.unsplash.com/photo-1522770179533-24471fcdba45",
-    title: "Camera",
-    author: "@helloimnik",
+    imageTitle: "Camera",
+    imageWriter: "@helloimnik",
   },
   {
     img: "https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c",
-    title: "Coffee",
-    author: "@nolanissac",
+    imageTitle: "Coffee",
+    imageWriter: "@nolanissac",
   },
   {
     img: "https://images.unsplash.com/photo-1533827432537-70133748f5c8",
-    title: "Hats",
-    author: "@hjrc33",
+    imageTitle: "Hats",
+    imageWriter: "@hjrc33",
   },
   {
     img: "https://images.unsplash.com/photo-1558642452-9d2a7deb7f62",
-    title: "Honey",
-    author: "@arwinneil",
+    imageTitle: "Honey",
+    imageWriter: "@arwinneil",
   },
   {
     img: "https://images.unsplash.com/photo-1516802273409-68526ee1bdd6",
-    title: "Basketball",
-    author: "@tjdragotta",
+    imageTitle: "Basketball",
+    imageWriter: "@tjdragotta",
   },
   {
     img: "https://images.unsplash.com/photo-1518756131217-31eb79b20e8f",
-    title: "Fern",
-    author: "@katie_wasserman",
+    imageTitle: "Fern",
+    imageWriter: "@katie_wasserman",
   },
   {
     img: "https://images.unsplash.com/photo-1597645587822-e99fa5d45d25",
-    title: "Mushrooms",
-    author: "@silverdalex",
+    imageTitle: "Mushrooms",
+    imageWriter: "@silverdalex",
   },
   {
     img: "https://images.unsplash.com/photo-1567306301408-9b74779a11af",
-    title: "Tomato basil",
-    author: "@shelleypauls",
+    imageTitle: "Tomato basil",
+    imageWriter: "@shelleypauls",
   },
   {
     img: "https://images.unsplash.com/photo-1471357674240-e1a485acb3e1",
-    title: "Sea star",
-    author: "@peterlaster",
+    imageTitle: "Sea star",
+    imageWriter: "@peterlaster",
   },
   {
     img: "https://images.unsplash.com/photo-1589118949245-7d38baf380d6",
-    title: "Bike",
-    author: "@southside_customs",
+    imageTitle: "Bike",
+    imageWriter: "@southside_customs",
   },
 ];
 
 const CategoryContainer = () => {
-  return <ImagePosts itemData={itemData} />;
+  const { category } = useParams();
+  return <ImagePosts itemData={itemData} category={category} />;
 };
 
 export default CategoryContainer;
