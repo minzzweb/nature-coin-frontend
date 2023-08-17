86d3696bc644ed0e698c661d3e311219484e490e
minzzweb <minzzweb@gmail.com>
2023-08-18 00:03:34 +0900

    메인 이미지 리스트 가져오기 구현

--- src/components/Home/HomeSection.js
+++ src/components/Home/HomeSection.js
@@ -1,4 +1,23 @@
+import React, { useEffect } from "react";
+import HomePosts from "./HomePosts";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  fetchMainImageList,
+  FETCH_MAINIMAGE_LIST,
+} from "../../modules/imageboard";
+
 const HomeSection = () => {
-  return <div>Section : 최근 이미지 사진들...</div>;
+  const dispatch = useDispatch();
+
+  const { images, isLoading } = useSelector(({ image, loading }) => ({
+    images: image.images,
+    isLoading: loading[FETCH_MAINIMAGE_LIST],
+  }));
+
+  useEffect(() => {
+    dispatch(fetchMainImageList());
+  }, [dispatch]);
+
+  return <HomePosts images={images} isLoading={isLoading} />;
 };
 export default HomeSection;
